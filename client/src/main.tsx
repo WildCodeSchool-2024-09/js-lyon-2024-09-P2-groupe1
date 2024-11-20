5f83327dc333e4c92d4c68f2698cb472d11dc02d
Cedric DAVASSOU <davassou.cedric.france@gmail.com>
2024-11-20 14:12:28 +0100

    Lien réseaux sociauxé

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -24,51 +24,6 @@ import Home from "./pages/Home";
 // Create router configuration with routes
 // You can add more routes as you build out your app!
 
-// const moviesData = [
-//   {
-//     title: "Les tomates de l'espace",
-//     image:
-//       "https://media.istockphoto.com/id/149049274/fr/photo/tomates-farcies.jpg?s=612x612&w=0&k=20&c=k4slxuM4taMJUkBeukW08kV2vC9vxmzsGe-50Ixay-Y=",
-//     description: "un film sur les tomates farcies",
-//     id: 1,
-//   },
-//   {
-//     title: "Les tomates de l'espace 2: le retour",
-//     image:
-//       "https://media.istockphoto.com/id/149049274/fr/photo/tomates-farcies.jpg?s=612x612&w=0&k=20&c=k4slxuM4taMJUkBeukW08kV2vC9vxmzsGe-50Ixay-Y=",
-//     description: "un deuxième film sur les tomates farcies",
-//     id: 2,
-//   },
-//   {
-//     title: "Les tomates de l'espace 3: la vengeance",
-//     image:
-//       "https://media.istockphoto.com/id/149049274/fr/photo/tomates-farcies.jpg?s=612x612&w=0&k=20&c=k4slxuM4taMJUkBeukW08kV2vC9vxmzsGe-50Ixay-Y=",
-//     description: "un troisième film sur les tomates farcies",
-//     id: 3,
-//   },
-//   {
-//     title: "Les tomates de l'espace 3: la vengeance",
-//     image:
-//       "https://media.istockphoto.com/id/149049274/fr/photo/tomates-farcies.jpg?s=612x612&w=0&k=20&c=k4slxuM4taMJUkBeukW08kV2vC9vxmzsGe-50Ixay-Y=",
-//     description: "un troisième film sur les tomates farcies",
-//     id: 4,
-//   },
-//   {
-//     title: "Les tomates de l'espace 3: la vengeance",
-//     image:
-//       "https://media.istockphoto.com/id/149049274/fr/photo/tomates-farcies.jpg?s=612x612&w=0&k=20&c=k4slxuM4taMJUkBeukW08kV2vC9vxmzsGe-50Ixay-Y=",
-//     description: "un troisième film sur les tomates farcies",
-//     id: 5,
-//   },
-//   {
-//     title: "Les tomates de l'espace 3: la vengeance",
-//     image:
-//       "https://media.istockphoto.com/id/149049274/fr/photo/tomates-farcies.jpg?s=612x612&w=0&k=20&c=k4slxuM4taMJUkBeukW08kV2vC9vxmzsGe-50Ixay-Y=",
-//     description: "un troisième film sur les tomates farcies",
-//     id: 6,
-//   },
-// ];
-
 const router = createBrowserRouter([
   {
     path: "/login",
@@ -129,5 +84,5 @@ createRoot(rootElement).render(
  *
  * 3. Experiment with Dynamic Routes:
  *    You can create routes that take parameters (e.g., /users/:id).
- *    Documentation: https://reactrouter.com/en/main/start/tutorial#url-params-in-loaders
+1 *    Documentation: https://reactrouter.com/en/main/start/tutorial#url-params-in-loaders
  */
