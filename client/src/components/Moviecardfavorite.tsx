cdf71b838a9ffbcb3115568033831faa5959713a
ALEXANDRE JANASIEWICZ <alexandrejanasiewicz@gmail.com>
2024-11-25 12:29:12 +0100

    commit avant pull

--- client/src/components/Moviecardfavorite.tsx
+++ client/src/components/Moviecardfavorite.tsx
@@ -5,9 +5,9 @@ import { type Movie, useFavorite } from "../Contexts/FavoriteContext";
 
 function MovieCardFavorite() {
   const { favorites, setFavorites, shows } = useFavorite();
-
+  
   const [showFavorite, setShowFavorite] = useState<Movie[]>([]);
-
+  
   useEffect(() => {
     const favoriteShows: Movie[] = shows.filter((show) =>
       favorites.includes(show.id),
